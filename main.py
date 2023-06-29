d12efefb42b380a5ddd1b7eb3b0955d5a8fda6df
solomon344 <willamssolomon672@gmail.com>
2023-06-29 01:56:55 +0000

    added a squareroot function to main.py in a different branch

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 
 def  add(num1,num2):
     return num1 + num2
@@ -10,3 +11,6 @@ def divide(num1,num2):
 
 def multiply(num1,num2):
     return num1 * num2
+
+def square(num):
+    return math.sqrt(num)

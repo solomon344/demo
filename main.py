4d707025bc781fa079ba17bf68fd5af7f06e7642
solomon344 <willamssolomon672@gmail.com>
2023-07-16 20:31:12 +0000

    commented out the subtract function in main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import math
 def  add(num1,num2):
     return num1 + num2
 
-def sub(num1,num2):
-    return num1 + num2
+#def sub(num1,num2):
+ #   return num1 + num2
 
 def divide(num1,num2):
     return num1 // num2
